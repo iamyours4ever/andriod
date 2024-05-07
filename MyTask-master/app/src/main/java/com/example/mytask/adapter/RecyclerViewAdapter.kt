29be5c76ec9d1dda78b8f805e2bf997d6700fec2
Roshan Lamichhane <roshankc423@gmail.com>
2024-05-07 22:44:22 +0545

    first commit

---
+++ MyTask-master/app/src/main/java/com/example/mytask/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.mytask.adapter
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mytask.R
+
+class RecyclerViewAdapter(var nameList: ArrayList<String>,
+                          var descriptionList: ArrayList<String>) : RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
+
+    class RecyclerViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        //bind views from layout file
+        var title : TextView = view.findViewById(R.id.title)
+        var description: TextView = view.findViewById(R.id.description)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        // when users opens activity this method links layout file to our activity
+        var view : View = LayoutInflater.from(parent.context).inflate(R.layout.sample_recycler_view,parent,false)
+        return RecyclerViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return nameList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        holder.title.text =  nameList[position]
+        holder.description.text =  descriptionList[position]
+        }
+}
